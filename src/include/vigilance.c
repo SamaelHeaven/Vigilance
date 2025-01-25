7ffb3e1b47ceb5da8cf7fb61d7237a6e0a19bfc3
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-01-25 02:58:26 -0500

    WIP

--- src/include/vigilance.c
+++ src/include/vigilance.c
@@ -9,10 +9,10 @@ void vigilance_main(Array_string args);
 int main(const int32_t argc, const char **argv) {
     GC_INIT();
     Array_string args = array_string_create();
-    array_string_reserve(&args, argc);
-    for (int32_t i = 0; i < argc; ++i) {
-        array_string_add(&args, string_create(argv[i]));
-    }
+   // array_string_reserve(&args, argc);
+   // for (int32_t i = 0; i < argc; ++i) {
+   //     array_string_add(&args, string_create(argv[i]));
+   // }
     vigilance_main(args);
     return 0;
 }
