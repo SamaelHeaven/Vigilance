ac25cb0e2a44cf2644ad86315600afc1a0c4a01e
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-01-25 03:53:44 -0500

    Fix: Using UTF8 console on windows

--- src/include/vigilance.c
+++ src/include/vigilance.c
@@ -2,12 +2,20 @@
 
 #include <gc.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #undef main
 
 void vigilance_main(Array_char_ptr args);
 
 int main(const int32_t argc, char **argv) {
     GC_INIT();
+#ifdef _WIN32
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+#endif
     Array_char_ptr args = array_char_ptr_create();
     array_char_ptr_reserve(&args, argc);
     for (int32_t i = 0; i < argc; ++i) {
