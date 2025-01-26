375c69fa25563362d647fb770538eaebbfbfcd22
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-01-25 23:00:16 -0500

    Fix: Minor fixes

--- src/core/parse.c
+++ src/core/parse.c
@@ -9,9 +9,10 @@ bool parse_bool(const char *char_ptr) {
 
 bool parse_int8(const char *char_ptr, int8_t *out) {
     assert(char_ptr && out);
+    errno = 0;
     char *end;
     const int32_t value = strtol(char_ptr, &end, 10);
-    if (*end != '\0' || errno == ERANGE || value < INT8_MIN || value > INT8_MAX) {
+    if (char_ptr == end || errno == ERANGE || *end || value < INT8_MIN || value > INT8_MAX) {
         return false;
     }
     *out = value;
@@ -20,9 +21,10 @@ bool parse_int8(const char *char_ptr, int8_t *out) {
 
 bool parse_int16(const char *char_ptr, int16_t *out) {
     assert(char_ptr && out);
+    errno = 0;
     char *end;
     const int32_t value = strtol(char_ptr, &end, 10);
-    if (*end != '\0' || errno == ERANGE || value < INT16_MIN || value > INT16_MAX) {
+    if (char_ptr == end || errno == ERANGE || *end || value < INT16_MIN || value > INT16_MAX) {
         return false;
     }
     *out = value;
@@ -31,9 +33,10 @@ bool parse_int16(const char *char_ptr, int16_t *out) {
 
 bool parse_int32(const char *char_ptr, int32_t *out) {
     assert(char_ptr && out);
+    errno = 0;
     char *end;
     const int32_t value = strtol(char_ptr, &end, 10);
-    if (*end != '\0' || errno == ERANGE || value < INT32_MIN || value > INT32_MAX) {
+    if (char_ptr == end || errno == ERANGE || *end || value < INT32_MIN || value > INT32_MAX) {
         return false;
     }
     *out = value;
@@ -42,9 +45,10 @@ bool parse_int32(const char *char_ptr, int32_t *out) {
 
 bool parse_int64(const char *char_ptr, int64_t *out) {
     assert(char_ptr && out);
+    errno = 0;
     char *end;
     const int64_t value = strtoll(char_ptr, &end, 10);
-    if (*end != '\0' || errno == ERANGE || value < LLONG_MIN || value > LLONG_MAX) {
+    if (char_ptr == end || errno == ERANGE || *end || value < LLONG_MIN || value > LLONG_MAX) {
         return false;
     }
     *out = value;
@@ -53,9 +57,10 @@ bool parse_int64(const char *char_ptr, int64_t *out) {
 
 bool parse_uint8(const char *char_ptr, uint8_t *out) {
     assert(char_ptr && out);
+    errno = 0;
     char *end;
     const uint32_t value = strtoul(char_ptr, &end, 10);
-    if (*end != '\0' || errno == ERANGE || value > UINT8_MAX) {
+    if (char_ptr == end || errno == ERANGE || *end || value > UINT8_MAX) {
         return false;
     }
     *out = value;
@@ -64,9 +69,10 @@ bool parse_uint8(const char *char_ptr, uint8_t *out) {
 
 bool parse_uint16(const char *char_ptr, uint16_t *out) {
     assert(char_ptr && out);
+    errno = 0;
     char *end;
     const uint32_t value = strtoul(char_ptr, &end, 10);
-    if (*end != '\0' || errno == ERANGE || value > UINT16_MAX) {
+    if (char_ptr == end || errno == ERANGE || *end || value > UINT16_MAX) {
         return false;
     }
     *out = value;
@@ -75,9 +81,10 @@ bool parse_uint16(const char *char_ptr, uint16_t *out) {
 
 bool parse_uint32(const char *char_ptr, uint32_t *out) {
     assert(char_ptr && out);
+    errno = 0;
     char *end;
     const uint32_t value = strtoul(char_ptr, &end, 10);
-    if (*end != '\0' || errno == ERANGE || value > UINT32_MAX) {
+    if (char_ptr == end || errno == ERANGE || *end || value > UINT32_MAX) {
         return false;
     }
     *out = value;
@@ -86,9 +93,10 @@ bool parse_uint32(const char *char_ptr, uint32_t *out) {
 
 bool parse_uint64(const char *char_ptr, uint64_t *out) {
     assert(char_ptr && out);
+    errno = 0;
     char *end;
     const uint64_t value = strtoull(char_ptr, &end, 10);
-    if (*end != '\0' || errno == ERANGE) {
+    if (char_ptr == end || errno == ERANGE || *end) {
         return false;
     }
     *out = value;
@@ -97,9 +105,10 @@ bool parse_uint64(const char *char_ptr, uint64_t *out) {
 
 bool parse_float(const char *char_ptr, float *out) {
     assert(char_ptr && out);
+    errno = 0;
     char *end;
     const float value = strtof(char_ptr, &end);
-    if (*end != '\0' || errno == ERANGE || value < -FLT_MAX || value > FLT_MAX) {
+    if (char_ptr == end || errno == ERANGE || *end || value < -FLT_MAX || value > FLT_MAX) {
         return false;
     }
     *out = value;
@@ -108,9 +117,10 @@ bool parse_float(const char *char_ptr, float *out) {
 
 bool parse_double(const char *char_ptr, double *out) {
     assert(char_ptr && out);
+    errno = 0;
     char *end;
     const double value = strtod(char_ptr, &end);
-    if (*end != '\0' || errno == ERANGE || value < -DBL_MAX || value > DBL_MAX) {
+    if (char_ptr == end || errno == ERANGE || *end || value < -DBL_MAX || value > DBL_MAX) {
         return false;
     }
     *out = value;
