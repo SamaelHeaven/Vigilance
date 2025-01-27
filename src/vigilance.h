5a78b5d9cab9dc42668664f3dad37735539b1922
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-01-27 15:09:35 -0500

    Refactor: Improved asserts

--- src/vigilance.h
+++ src/vigilance.h
@@ -22,19 +22,12 @@
 
 #define CAST(type, value) (*(type *) &LVALUE(value))
 
-#define BASENAME(file)                                                                                                 \
-    ({                                                                                                                 \
-        char *basename_ = strrchr(file, '/');                                                                          \
-        basename_ = basename_ ? basename_ : strrchr(file, '\\');                                                       \
-        basename_ ? basename_ + 1 : file;                                                                              \
-    })
-
 #ifdef NDEBUG
 #define ASSERT(expr) ((void) 0)
 #else
 #define ASSERT(expr)                                                                                                   \
     ((expr) ? (void) 0                                                                                                 \
-            : (fprintf(stderr, "[ASSERTION FAILED] %s:%d %s(): %s\n    at %s\n", BASENAME(__FILE__), __LINE__,         \
-                       __func__, #expr, __FILE__),                                                                     \
+            : (fprintf(stderr, "[ASSERTION FAILED] %s:%d %s(): %s\n    at %s\n", strrchr("/"__FILE__, '/') + 1,        \
+                       __LINE__, __func__, #expr, __FILE__),                                                           \
                abort()))
 #endif
