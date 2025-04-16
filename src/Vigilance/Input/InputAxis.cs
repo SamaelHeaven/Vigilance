dc3a26ece158d32c9f211edcedc4aead9bf4bf91
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-04-15 22:06:20 -0400

    Feat: Added dead zone in input axis

--- src/Vigilance/Input/InputAxis.cs
+++ src/Vigilance/Input/InputAxis.cs
@@ -11,6 +11,7 @@ public struct InputAxis
     public IReadOnlyList<GamepadButton> PositiveGamepadButtons { get; set; } = Array.Empty<GamepadButton>();
     public IReadOnlyList<GamepadAxis> GamepadAxes { get; set; } = Array.Empty<GamepadAxis>();
     public Gamepad Gamepad { get; set; } = Gamepad.First;
+    public float DeadZone { get; set; } = 0;
 
     public static InputAxis Horizontal => Game.HorizontalInputAxis;
 
@@ -26,14 +27,15 @@ public struct InputAxis
     public readonly int Get()
     {
         var gamepad = Gamepad;
+        var deadZone = DeadZone;
         var negative =
             NegativeKeys.Any(Keyboard.IsKeyDown)
             || NegativeGamepadButtons.Any(button => gamepad.IsButtonDown(button))
-            || GamepadAxes.Any(axis => (int)MathF.Round(gamepad.GetAxis(axis)) == -1);
+            || GamepadAxes.Any(axis => (int)MathF.Round(gamepad.GetAxis(axis) - deadZone) <= -1);
         var positive =
             PositiveKeys.Any(Keyboard.IsKeyDown)
             || PositiveGamepadButtons.Any(button => gamepad.IsButtonDown(button))
-            || GamepadAxes.Any(axis => (int)MathF.Round(gamepad.GetAxis(axis)) == 1);
+            || GamepadAxes.Any(axis => (int)MathF.Round(gamepad.GetAxis(axis) + deadZone) >= 1);
         if (negative && !positive)
             return -1;
         if (positive && !negative)
