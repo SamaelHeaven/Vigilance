797d14882cb5cfc61b7c0b26a84c2e572626369a
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-04-18 18:02:26 -0400

    Refactor: Minor changes

--- src/Vigilance/Drawing/TextureAtlas.cs
+++ src/Vigilance/Drawing/TextureAtlas.cs
@@ -5,16 +5,20 @@ namespace Vigilance.Drawing;
 
 public readonly struct TextureAtlas : IEnumerable<Box>
 {
-    private readonly List<Box> _boxes;
+    private readonly Box[] _boxes;
     public Texture Texture { get; }
     public Vector2 RegionSize { get; }
     public float Spacing { get; }
 
-    public Vector2 Size => Texture.Size;
+    public int Cols => (int)(TextureWidth / (RegionWidth + Spacing));
 
-    public float Width => Size.X;
+    public int Rows => (int)MathF.Ceiling(Count / (float)Cols);
 
-    public float Height => Size.Y;
+    public Vector2 TextureSize => Texture.Size;
+
+    public float TextureWidth => TextureSize.X;
+
+    public float TextureHeight => TextureSize.Y;
 
     public float RegionWidth => RegionSize.X;
 
@@ -22,33 +26,33 @@ public readonly struct TextureAtlas : IEnumerable<Box>
 
     public Box this[int index] => GetRegion(index);
 
-    public Box this[int col, int row] => GetRegion(col, row);
+    public Box this[int x, int y] => GetRegion(x, y);
 
     public Box this[Vector2 position] => GetRegion(position);
 
-    public int Count => _boxes.Count;
+    public int Count => _boxes.Length;
 
-    public TextureAtlas(Texture texture, Vector2 nbRegion, float spacing = 0)
-        : this(texture, (int)nbRegion.X, (int)nbRegion.Y, spacing) { }
+    public TextureAtlas(Texture texture, Vector2 count, float spacing = 0)
+        : this(texture, (int)count.X, (int)count.Y, spacing) { }
 
-    public TextureAtlas(Texture texture, int nbCols, int nbRows, float spacing = 0)
-        : this(texture, texture.Width / (float)nbCols, texture.Height / (float)nbRows, nbCols * nbRows, spacing) { }
+    public TextureAtlas(Texture texture, int cols, int rows, float spacing = 0)
+        : this(texture, texture.Width / (float)cols, texture.Height / (float)rows, cols * rows, spacing) { }
 
-    public TextureAtlas(Texture texture, Vector2 regionSize, int nbRegion, float spacing = 0)
-        : this(texture, regionSize.X, regionSize.Y, nbRegion, spacing) { }
+    public TextureAtlas(Texture texture, Vector2 regionSize, int count, float spacing = 0)
+        : this(texture, regionSize.X, regionSize.Y, count, spacing) { }
 
-    public TextureAtlas(Texture texture, float regionWidth, float regionHeight, int nbRegion, float spacing = 0)
+    public TextureAtlas(Texture texture, float regionWidth, float regionHeight, int count, float spacing = 0)
     {
-        _boxes = new List<Box>(nbRegion);
+        _boxes = new Box[count];
         Texture = texture;
         RegionSize = new Vector2(regionWidth, regionHeight);
         Spacing = spacing;
         var offsetX = 0.0f;
         var offsetY = 0.0f;
-        for (var i = 0; i < nbRegion; i++)
+        for (var i = 0; i < count; i++)
         {
             var box = new Box(offsetX, offsetY, regionWidth, regionHeight);
-            _boxes.Add(box);
+            _boxes[i] = box;
             offsetX += regionWidth + spacing;
             if (!(offsetX + regionWidth > texture.Width))
                 continue;
@@ -79,13 +83,12 @@ public readonly struct TextureAtlas : IEnumerable<Box>
 
     public int GetIndex(int col, int row)
     {
-        var cols = (int)(Width / (RegionWidth + Spacing));
-        return row * cols + col;
+        return row * Cols + col;
     }
 
     public IEnumerator<Box> GetEnumerator()
     {
-        return _boxes.GetEnumerator();
+        return _boxes.Cast<Box>().GetEnumerator();
     }
 
     IEnumerator IEnumerable.GetEnumerator()
