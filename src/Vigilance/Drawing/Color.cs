47e8443e2603f4197aecd4d3115542d6108336c5
Samuel Paradis <126531890+SamaelHeaven@users.noreply.github.com>
2025-09-25 11:10:37 -0400

    Refactor: Minor changes

--- src/Vigilance/Drawing/Color.cs
+++ src/Vigilance/Drawing/Color.cs
@@ -9,7 +9,7 @@ public record struct Color
 {
     public static Color White => new(255, 255, 255);
     public static Color Black => new(0, 0, 0);
-    public static Color Transparent => new(0, 0, 0, 0);
+    public static Color Transparent => default;
     public static Color LightGray => new(200, 200, 200);
     public static Color Gray => new(130, 130, 130);
     public static Color DarkGray => new(80, 80, 80);
