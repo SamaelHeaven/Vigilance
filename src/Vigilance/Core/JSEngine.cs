db77a57fc1a198bce55d343fb83cd6d200eb9351
Samuel Paradis <126531890+SamaelHeaven@users.noreply.github.com>
2026-02-17 17:57:45 -0500

    Refactor: Minor changes

--- src/Vigilance/Core/JSEngine.cs
+++ src/Vigilance/Core/JSEngine.cs
@@ -22,67 +22,67 @@ public readonly record struct JSResult
 
     public static implicit operator char(JSResult result)
     {
-        return char.Parse(result);
+        return char.TryParse(result, out var value) ? value : '\0';
     }
 
     public static implicit operator bool(JSResult result)
     {
-        return bool.Parse(result);
+        return bool.TryParse(result, out var value) && value;
     }
 
     public static implicit operator sbyte(JSResult result)
     {
-        return sbyte.Parse(result);
+        return sbyte.TryParse(result.Value, out var value) ? value : (sbyte)0;
     }
 
     public static implicit operator short(JSResult result)
     {
-        return short.Parse(result);
+        return short.TryParse(result.Value, out var value) ? value : (short)0;
     }
 
     public static implicit operator int(JSResult result)
     {
-        return int.Parse(result);
+        return int.TryParse(result.Value, out var value) ? value : 0;
     }
 
     public static implicit operator long(JSResult result)
     {
-        return long.Parse(result);
+        return long.TryParse(result.Value, out var value) ? value : 0;
     }
 
     public static implicit operator byte(JSResult result)
     {
-        return byte.Parse(result);
+        return byte.TryParse(result.Value, out var value) ? value : (byte)0;
     }
 
     public static implicit operator ushort(JSResult result)
     {
-        return ushort.Parse(result);
+        return ushort.TryParse(result.Value, out var value) ? value : (ushort)0;
     }
 
     public static implicit operator uint(JSResult result)
     {
-        return uint.Parse(result);
+        return uint.TryParse(result.Value, out var value) ? value : 0;
     }
 
     public static implicit operator ulong(JSResult result)
     {
-        return ulong.Parse(result);
+        return ulong.TryParse(result.Value, out var value) ? value : 0;
     }
 
     public static implicit operator float(JSResult result)
     {
-        return float.Parse(result);
+        return float.TryParse(result.Value, out var value) ? value : 0;
     }
 
     public static implicit operator double(JSResult result)
     {
-        return double.Parse(result);
+        return double.TryParse(result.Value, out var value) ? value : 0;
     }
 
     public static implicit operator decimal(JSResult result)
     {
-        return decimal.Parse(result);
+        return decimal.TryParse(result.Value, out var value) ? value : 0;
     }
 
     public override string ToString()
