ea663c1acbd3d1ef0f41c8ba17bd107b178a5db8
Samuel Paradis <126531890+SamaelHeaven@users.noreply.github.com>
2026-06-25 14:37:57 -0400

    Feat: UI sprite animation

--- src/Vigilance/UI/UISprite.cs
+++ src/Vigilance/UI/UISprite.cs
@@ -75,3 +75,41 @@ public class UISprite : UIContainer
         _sprite = _sprite.DeepClone();
     }
 }
+
+public static class UISpriteExtensions
+{
+    extension(SpriteAnimationFrame frame)
+    {
+        public void UpdateSprite(UISprite sprite)
+        {
+            if (frame.Texture is not null)
+                sprite.Texture = frame.Texture;
+            if (frame.FlipX.HasValue)
+                sprite.FlipX = frame.FlipX.Value;
+            if (frame.FlipY.HasValue)
+                sprite.FlipY = frame.FlipY.Value;
+            if (frame.Source.HasValue)
+                sprite.Source = frame.Source;
+            if (frame.Tint.HasValue)
+                sprite.Tint = frame.Tint.Value;
+            if (frame.NPatchInfo.HasValue)
+                sprite.NPatchInfo = frame.NPatchInfo;
+            if (frame.Interpolation.HasValue)
+                sprite.Interpolation = frame.Interpolation.Value;
+            if (frame.Position.HasValue)
+                sprite.Translate = frame.Position.Value;
+            if (frame.Scale.HasValue)
+                sprite.Scale = frame.Scale.Value;
+            if (frame.Rotation.HasValue)
+                sprite.Rotation = frame.Rotation.Value;
+            if (frame.PivotPoint.HasValue)
+                sprite.PivotPoint = frame.PivotPoint.Value;
+            if (frame.BlendMode.HasValue)
+                sprite.BlendMode = frame.BlendMode.Value;
+            if (frame.Shader.HasValue)
+                sprite.Shader = frame.Shader;
+            if (frame.Culling.HasValue)
+                sprite.Culling = frame.Culling.Value;
+        }
+    }
+}
