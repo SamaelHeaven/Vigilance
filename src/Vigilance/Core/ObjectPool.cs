7c6878b0f2789942728fb81d5ccf88533096e3de
Samuel Paradis <126531890+SamaelHeaven@users.noreply.github.com>
2026-06-27 01:18:14 -0400

    Fix: Minor fix

--- src/Vigilance/Core/ObjectPool.cs
+++ src/Vigilance/Core/ObjectPool.cs
@@ -1,22 +1,29 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using Vigilance.Collections;
+using BindingFlags = System.Reflection.BindingFlags;
 
 namespace Vigilance.Core;
 
 [SuppressMessage("ReSharper", "StaticMemberInGenericType")]
-public static class ObjectPool<
+public static unsafe class ObjectPool<
     [DynamicallyAccessedMembers(
         DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.NonPublicConstructors
     )]
-    T
+        T
 >
     where T : class, new()
 {
-    private static readonly T _default = new();
     private static readonly T _uninitialized = (T)RuntimeHelpers.GetUninitializedObject(typeof(T));
+    private static readonly nuint _size = GetRawObjectDataSize(null, _uninitialized);
+    private static readonly delegate* <object?, void> _constructor;
     private static ValueStack<T> _pool = [];
-    private static readonly nuint _size = GetRawObjectDataSize(null, _default);
+
+    static ObjectPool()
+    {
+        var constructor = typeof(T).GetConstructor(BindingFlags.Instance | BindingFlags.Public, [])!;
+        _constructor = (delegate* <object?, void>)constructor.MethodHandle.GetFunctionPointer();
+    }
 
     public static int Count => _pool.Count;
 
@@ -28,15 +35,13 @@ public static class ObjectPool<
 
     [UnsafeAccessor(UnsafeAccessorKind.StaticMethod)]
     private static extern ref byte GetRawData(
-        [UnsafeAccessorType("System.Runtime.CompilerServices.RuntimeHelpers")]
-        object? type,
+        [UnsafeAccessorType("System.Runtime.CompilerServices.RuntimeHelpers")] object? type,
         object obj
     );
 
     [UnsafeAccessor(UnsafeAccessorKind.StaticMethod)]
     private static extern nuint GetRawObjectDataSize(
-        [UnsafeAccessorType("System.Runtime.CompilerServices.RuntimeHelpers")]
-        object? type,
+        [UnsafeAccessorType("System.Runtime.CompilerServices.RuntimeHelpers")] object? type,
         object obj
     );
 
@@ -53,10 +58,13 @@ public static class ObjectPool<
         if (_pool.Count == 0)
             return new T();
         var item = _pool.Pop();
-        ref var data = ref GetRawData(null, item);
-        ref var defaultData = ref GetRawData(null, _default);
-        BulkMoveWithWriteBarrier(null, ref data, ref defaultData, _size);
+        _constructor(item);
         return item;
+    }
+
+    public static Handle Borrow()
+    {
+        return new Handle(Rent());
     }
 
     public static void Return(T item)
@@ -79,5 +87,25 @@ public static class ObjectPool<
     public static void TrimExcess()
     {
         _pool.TrimExcess();
+    }
+
+    public readonly ref struct Handle : IDisposable
+    {
+        public T Value { get; }
+
+        internal Handle(T value)
+        {
+            Value = value;
+        }
+
+        public void Dispose()
+        {
+            Return(Value);
+        }
+
+        public static implicit operator T(Handle handle)
+        {
+            return handle.Value;
+        }
     }
 }
